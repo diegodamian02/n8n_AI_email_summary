f895e1ff461663e4e8d27cdbda825cf6923c68e5
Diego Damian <dad405@scarletmail.rutgers.edu>
2025-05-15 15:00:28 -0400

    api integration, right output

--- summarize_email.py
+++ summarize_email.py
@@ -1,21 +1,24 @@
-import sys
-import json
+
+from flask import Flask, request, jsonify
 import google.generativeai as genai
+import os
 
-email_body = sys.stdin.read()
-
-genai.configure(api_key="AIzaSyACVKbyIjtXbf_e6QzX_r17bTKVx6fougs")
+app = Flask(__name__)
+genai.configure(api_key="AIzaSyDg2yDd4XKcHdRkDfYDEtYL5mjB02E-PSg")
 
 model = genai.GenerativeModel("gemini-2.0-flash")
 
-#prompt
-response = model.generate_content(
-    f"Summarize the following email in 3-4 sentences:\n\n{email_body}"
-)
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.get_json()
+    email_body = data.get("email_body", "")
+    response = model.generate_content(
+        f"Summarize the following email in 3-4 sentences:\n\n{email_body}"
+        )
 
-#extract the generated summary
+    return jsonify({"summary:": response.text.strip()})
 
-summary = response.text.strip()
+if __name__ == '__main__':
+    app.run(debug=True)
 
-#output as JSON
-print(json.dumps({"summary": summary}))
+
